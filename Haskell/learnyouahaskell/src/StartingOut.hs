c9291e4449edb1e483d309854733086b5fc06b40
Ata Niyazov <ata.niazov@gmail.com>
2018-03-06 05:05:36 +0300

    continue of learnyouahaskell

--- Haskell/learnyouahaskell/src/StartingOut.hs
+++ Haskell/learnyouahaskell/src/StartingOut.hs
@@ -27,8 +27,8 @@ startingOut = do
   print (5 + 4.0)
   -- multiply (*) infix function
   print (succ 8)
-  -- print (5 + "llama")
-  -- print (5 == True)
+  --print (5 + "llama")
+  --print (5 == True)
   print (min 9 10)
   print (min 3.4 3.2)
   print (max 100 101)
@@ -52,7 +52,79 @@ startingOut = do
   -- An intro to lists
   -- Lists are a homogenous data structure,
   -- it stores several elements of the same type.
-  print (lostNumbers)
+  --a = 1
+  let a = 1
+  print a
+  let lostNumbers = [4, 8, 15, 16, 23, 42]
+  print lostNumbers
+  print ['h', 'e', 'l', 'l', 'o']
+  print "world"
+  -- ++ operator
+  print $ [1, 2, 3, 4] ++ [9, 10, 11, 12]
+  print $ "hello" ++ " " ++ "world"
+  print $ ['w', 'o'] ++ ['o', 't']
+  -- bad performance
+  print $ [1, 2, 3] ++ [4]
+  -- use cons (:) operator
+  print $ 'A' : " SMALL CAT"
+  print $ 5 : [1, 2, 3, 4, 5]
+  print [1, 2, 3]
+  print $ 1 : 2 : 3 : []
+  -- [] empty list
+  let alist = []
+  print $ alist ++ [3] ++ [2]
+  -- [], [[]], [[],[],[]]
+  -- !! get an element out of a list by index
+  print $ "Steve Buscemi" !! 6
+  print $ [9.4, 33.2, 96.2, 11.2, 23.25] !! 1
+  --print $ [9.4, 33.2, 96.2, 11.2, 23.25] !! 5
+  let b = [[1, 2, 3, 4], [5, 3, 3, 3], [1, 2, 2, 3, 4], [1, 2, 3]]
+  print b
+  print $ b ++ [[1, 1, 1, 1]]
+  print $ [6, 6, 6] : b
+  print $ b !! 2
+  print $ [3, 2, 1] > [2, 1, 0]
+  print $ [3, 2, 1] > [2, 10, 100]
+  print $ [3, 4, 2] > [3, 4]
+  print $ [3, 4, 2] > [2, 4]
+  print $ [3, 4, 2] == [3, 4, 2]
+  -- basic functions that operate on lists
+  -- head, tail, last, init
+  print $ head [5, 4, 3, 2, 1]
+  print $ tail [5, 4, 3, 2, 1]
+  print $ last [5, 4, 3, 2, 1]
+  print $ init [5, 4, 3, 2, 1]
+  --print $ head $ tail [0]
+  print $ head [0]
+  print $ tail [0]
+  print $ init [0]
+  print $ last [0]
+  -- length, null
+  print $ length [5, 4, 3, 2, 1]
+  print $ null [1, 2, 3]
+  print $ null []
+  -- reverse, take, drop
+  print $ reverse [5, 4, 3, 2, 1]
+  print $ take 3 [5, 4, 3, 2, 1]
+  print $ take 1 [3, 9, 3]
+  print $ take 5 [1, 2]
+  print $ take 0 [6, 6, 6]
+  print $ drop 3 [8, 4, 2, 1, 5, 6]
+  print $ drop 0 [1, 2, 3, 4]
+  print $ drop 100 [1, 2, 3, 4]
+  -- minimum, maximum
+  print $ minimum [8, 4, 2, 1, 5, 6]
+  print $ maximum [1, 9, 2, 3, 4]
+  -- sum, product, elem
+  print $ sum [5, 2, 1, 6, 3, 2, 5, 7]
+  print $ sum [-1, -2, -3, -4]
+  print $ product [6, 2, 1, 2]
+  print $ product [1, 2, 5, 6, 7, 9, 2, 0]
+  -- elem
+  print $ 4 `elem` [3,4,5,6]
+  print $ 10 `elem` [3,4,5,6]
+  print $ elem 1 []
+  -- Texas ranges
 
 doubleMe x = x + x
 
@@ -78,3 +150,4 @@ doubleSmallNumber' x =
 conanO'Brien = "It's a-me, Conan O'Brien!"
 
 lostNumbers = [4, 8, 15, 16, 23, 42]
+-- [1,2,'a',3,'b','c',4]
